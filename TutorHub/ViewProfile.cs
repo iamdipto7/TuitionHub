170a5110c649086760383aaeb8a0d07b5db5c4cd
Baki Forhad <bakiforhad11@gmail.com>
2017-08-19 01:56:55 +0600

    view problem solved

--- TutorHub/ViewProfile.cs
+++ TutorHub/ViewProfile.cs
@@ -53,7 +53,7 @@ namespace TutorHub
         {
             this.metroTabControl1.SelectedIndex = 0;
 
-            if (Form1.Instance.LoggedInUser.UserName.Equals(""))
+            if (Form1.Instance.LoggedInUser.UserName == null)
             {
                 metroLabel6.Text = metroLabel5.Text = Form1.Instance.LoggedInUser.FirstName +" " +Form1.Instance.LoggedInUser.LastName;
 
@@ -63,7 +63,7 @@ namespace TutorHub
                 metroLabel6.Text = metroLabel5.Text = Form1.Instance.LoggedInUser.UserName;
             }
 
-            if (Form1.Instance.LoggedInUser.FirstName =="" || Form1.Instance.LoggedInUser.FirstName == "")
+            if ((Form1.Instance.LoggedInUser.FirstName == null) || (Form1.Instance.LoggedInUser.FirstName== null))
             {
                 lblName.Text = Form1.Instance.LoggedInUser.UserName;
             }
@@ -72,18 +72,18 @@ namespace TutorHub
                 lblName.Text = Form1.Instance.LoggedInUser.FirstName + " " + Form1.Instance.LoggedInUser.LastName + "( " + Form1.Instance.LoggedInUser.UserName + " )";
             }
 
-            if(Form1.Instance.LoggedInUser.DOB.ToString().Equals(""))
+            if(Form1.Instance.LoggedInUser.DOB.ToString()== null)
             {
                 lblDOB.Text = "Not provded";
 
             }
             else
             {
-                lblDOB.Text = Form1.Instance.LoggedInUser.DOB.ToString();
+                lblDOB.Text = Form1.Instance.LoggedInUser.DOB.Value.Date.ToString();
             }
 
 
-            if (Form1.Instance.LoggedInUser.Gender.Equals(""))
+            if (Form1.Instance.LoggedInUser.Gender == null)
             {
                 lblGender.Text = "Not provded";
 
@@ -94,7 +94,7 @@ namespace TutorHub
             }
 
             
-             if(Form1.Instance.LoggedInUser.SkypeId.Equals(""))
+             if(Form1.Instance.LoggedInUser.SkypeId== null)
             {
                 metroLabel13.Text = "Not provded";
 
@@ -105,7 +105,7 @@ namespace TutorHub
             }
 
 
-            if (Form1.Instance.LoggedInUser.Description.Equals(""))
+            if (Form1.Instance.LoggedInUser.Description== null)
             {
                 metroTile2.Text = "Not provded";
 
@@ -116,7 +116,7 @@ namespace TutorHub
             }
 
 
-            if (Form1.Instance.LoggedInUser.phone.Equals(""))
+            if (Form1.Instance.LoggedInUser.phone== null)
             {
                 metroLabel14.Text = "Not provided";
             }
@@ -126,7 +126,7 @@ namespace TutorHub
             }
 
 
-            if (Form1.Instance.LoggedInUser.Address.Equals(""))
+            if (Form1.Instance.LoggedInUser.Address== null)
             {
                 metroLabel15.Text = "Not provided";
             }
@@ -136,15 +136,15 @@ namespace TutorHub
             }
 
 
-            if (Form1.Instance.LoggedInUser.Versity.Equals(""))
+            if (Form1.Instance.LoggedInUser.Versity== null)
             {
                 metroLabel19.Text = "Not provided";
             }
             else
             {
-                metroLabel19.Text = Form1.Instance.LoggedInUser.Versity ;
-                
-                if(Form1.Instance.LoggedInUser.Department.Equals(""))
+                metroLabel19.Text = Form1.Instance.LoggedInUser.Versity;
+
+                if (Form1.Instance.LoggedInUser.Department== null)
                 {
                     metroLabel19.Text += "\n Dept: not provided";
                 }
@@ -154,20 +154,20 @@ namespace TutorHub
                 }
 
 
-                if(Form1.Instance.LoggedInUser.Semister.Equals(""))
+                if (Form1.Instance.LoggedInUser.Semister== null)
                 {
                     metroLabel19.Text += "\n Semister: Not provided";
                 }
                 else
                 {
-                    metroLabel19.Text += "\n Semister: "+ Form1.Instance.LoggedInUser.Semister;
+                    metroLabel19.Text += "\n Semister: " + Form1.Instance.LoggedInUser.Semister;
 
                 }
 
             }
 
 
-            if (Form1.Instance.LoggedInUser.Email.Equals(""))
+            if (Form1.Instance.LoggedInUser.Email== null)
             {
                 metroLabel2.Text = "Not provided";
             }
@@ -180,7 +180,7 @@ namespace TutorHub
 
 
 
-            if (Form1.Instance.LoggedInUser.SSC.Equals(""))
+            if (Form1.Instance.LoggedInUser.SSC== null)
             {
                 metroLabel21.Text = "Not provided";
             }
@@ -188,17 +188,17 @@ namespace TutorHub
             {
                 metroLabel21.Text = Form1.Instance.LoggedInUser.SSC;
 
-                if (!Form1.Instance.LoggedInUser.SSCYear.Equals(""))
+                if (!(Form1.Instance.LoggedInUser.SSCYear== null))
                 {
 
-                    metroLabel21.Text += " ( " + Form1.Instance.LoggedInUser.SSCYear+ " )";
+                    metroLabel21.Text += " ( " + Form1.Instance.LoggedInUser.SSCYear + " )";
                 }
 
             }
 
 
 
-            if (Form1.Instance.LoggedInUser.HSC.Equals(""))
+            if (Form1.Instance.LoggedInUser.HSC== null)
             {
                 metroLabel20.Text = "Not provided";
             }
@@ -206,7 +206,7 @@ namespace TutorHub
             {
                 metroLabel20.Text = Form1.Instance.LoggedInUser.HSC;
 
-                if (!Form1.Instance.LoggedInUser.HSCYear.Equals(""))
+                if (!(Form1.Instance.LoggedInUser.HSCYear == null))
                 {
 
                     metroLabel20.Text += " ( " + Form1.Instance.LoggedInUser.HSCYear + " )";
@@ -233,6 +233,216 @@ namespace TutorHub
                     metroLabel22.Text += s.SkillName + " ;";
                 }
                 metroLabel22.Text.Remove(metroLabel22.Text.Length-2, 2);
+            }
+
+
+
+
+        }
+
+
+
+
+        //below this is for other
+        public void ViewOtherProfile(int x)
+        {
+            int refer = x;
+            TutorHubDataContext td = new TutorHubDataContext(Login.Instance.connection);
+            User profile = td.Users.SingleOrDefault(z => z.UserId == refer);
+
+            //User profile=(User)v;
+            
+           
+
+            
+
+           
+            ViewProfile.Instance.BringToFront();
+
+
+
+            this.metroTabControl1.SelectedIndex = 0;
+
+            if (profile.UserName== null)
+            {
+                metroLabel6.Text = metroLabel5.Text = profile.FirstName + " " + profile.LastName;
+
+            }
+            else
+            {
+                metroLabel6.Text = metroLabel5.Text = profile.UserName;
+            }
+
+            if (profile.FirstName == "" || profile.FirstName == "")
+            {
+                lblName.Text = profile.UserName;
+            }
+            else
+            {
+                lblName.Text = profile.FirstName + " " + profile.LastName + "( " + profile.UserName + " )";
+            }
+
+            if (profile.ToString()== null)
+            {
+                lblDOB.Text = "Not provded";
+
+            }
+            else
+            {
+                lblDOB.Text = profile.DOB.Value.Date.ToString();
+            }
+
+
+            if ( profile.Gender == null)
+            {
+                lblGender.Text = "Not provded";
+
+            }
+            else
+            {
+                lblGender.Text = profile.Gender;
+            }
+
+
+            if (profile.SkypeId== null)
+            {
+                metroLabel13.Text = "Not provded";
+
+            }
+            else
+            {
+                metroLabel13.Text = profile.SkypeId;
+            }
+
+
+            if (profile.Description== null)
+            {
+                metroTile2.Text = "Not provded";
+
+            }
+            else
+            {
+                metroTile2.Text = profile.Description;
+            }
+
+
+            if (profile.phone== null)
+            {
+                metroLabel14.Text = "Not provided";
+            }
+            else
+            {
+                metroLabel14.Text = profile.phone;
+            }
+
+
+            if (profile.Address== null)
+            {
+                metroLabel15.Text = "Not provided";
+            }
+            else
+            {
+                metroLabel15.Text = profile.Address;
+            }
+
+
+            if (profile.Versity== null)
+            {
+                metroLabel19.Text = "Not provided";
+            }
+            else
+            {
+                metroLabel19.Text = profile.Versity;
+
+                if (profile.Department== null)
+                {
+                    metroLabel19.Text += "\n Dept: not provided";
+                }
+                else
+                {
+                    metroLabel19.Text += "\n Dept: " + profile.Department;
+                }
+
+
+                if (profile.Semister== null)
+                {
+                    metroLabel19.Text += "\n Semister: Not provided";
+                }
+                else
+                {
+                    metroLabel19.Text += "\n Semister: " + profile.Semister;
+
+                }
+
+            }
+
+
+            if (profile.Email== null)
+            {
+                metroLabel2.Text = "Not provided";
+            }
+            else
+            {
+                metroLabel12.Text = profile.Email;
+
+            }
+
+
+
+
+            if (profile.SSC== null)
+            {
+                metroLabel21.Text = "Not provided";
+            }
+            else
+            {
+                metroLabel21.Text = profile.SSC;
+
+                if (!(profile.SSCYear== null))
+                {
+
+                    metroLabel21.Text += " ( " + profile.SSCYear + " )";
+                }
+
+            }
+
+
+
+            if (profile.HSC== null)
+            {
+                metroLabel20.Text = "Not provided";
+            }
+            else
+            {
+                metroLabel20.Text = profile.HSC;
+
+                if (!(profile.HSCYear == null))
+                {
+
+                    metroLabel20.Text += " ( " + profile.HSCYear + " )";
+                }
+
+            }
+
+            TutorHubDataContext tdx = new TutorHubDataContext(Login.Instance.connection);
+
+            var skls = from z in tdx.Skills
+                       where z.UserId == profile.UserId
+                       select z;
+
+            if (skls.Count() == 0)
+            {
+                metroLabel22.Text = "No skills provided";
+            }
+            else
+            {
+                metroLabel22.Text = "";
+                foreach (var s in skls)
+                {
+
+                    metroLabel22.Text += s.SkillName + " ;";
+                }
+                metroLabel22.Text.Remove(metroLabel22.Text.Length - 2, 2);
             }
 
 
